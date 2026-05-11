89330ebe9e9d8dd3c498826ccdf48a01e5bd545d
vasiliysonich-web <vasiliysonich@gmail.com>
2026-05-11 10:55:46 +1000

    Update page.tsx
    убрал блок на главной странице

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
-import Navbar from '@/components/navigation/navbar'
-import Hero from '@/components/sections/hero'
-import About from '@/components/sections/about'
-import Services from '@/components/sections/services'
-import Cases from '@/components/sections/cases'
-import Reviews from '@/components/sections/reviews'
-import Contact from '@/components/sections/contact'
-import { Footer } from '@/components/layout/footer'
+import Navbar from "@/components/navigation/navbar";
+import Hero from "@/components/sections/hero";
+import About from "@/components/sections/about";
+import Services from "@/components/sections/services";
+import Contact from "@/components/sections/contact";
+import { Footer } from "@/components/layout/footer";
 
 export default function Home() {
   return (
@@ -14,10 +12,8 @@ export default function Home() {
       <Hero />
       <About />
       <Services />
-      <Cases />
-      <Reviews />
       <Contact />
       <Footer />
     </main>
-  )
+  );
 }
